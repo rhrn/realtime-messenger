ce81e893cbf0236cbb64afac6ef72f416f1b70ac
rhrn <maylog@gmail.com>
2014-05-04 23:01:04 +0400

    Add application server routes

---
+++ routes/index.js
@@ -0,0 +1,42 @@
+var rootHandler = function (request, reply) {
+
+  reply.view('index', {
+    pageTitle: 'Chat entry point',
+    room: null,
+  });
+
+};
+
+var roomHandler = function (request, reply) {
+
+  var room = request.params.room;
+  if (!room.match(/[a-z0-9]+/i)) {
+    room = 'main';
+  }
+
+  reply.view('index', {
+    pageTitle: 'room: ' + room,
+    room: room
+  });
+
+};
+
+module.exports = [
+  {
+    method: 'GET',
+    path: '/static/{path*}',
+    handler: {
+      directory: {path: './public/static', listing: false, index: true}
+    }
+  },
+  {
+    method: 'GET',
+    path: '/',
+    handler: rootHandler
+  },
+  {
+    method: 'GET',
+    path: '/chat/room/{room*}',
+    handler: roomHandler
+  }
+];
